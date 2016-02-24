d0d0fb22fbfe5276595174c1fe4f9b8fedcae0ed
no3076944 <david.furfaro@yahoo.com>
2016-02-24 15:19:13 -0500

    Assignment 3 Commit

---
+++ code/myBlinkingLed.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.OUT)
+
+
+def Blink():
+
+	while True:
+		GPIO.output(17,True)
+		time.sleep(1)
+		GPIO.output(17,False)
+		time.sleep(1)
+		GPIO.output(17,True)
+		time.sleep(1)
+		GPIO.output(17,False)
+		time.sleep(1)
+		GPIO.output(17,True)
+		time.sleep(1)
+		GPIO.output(17,False)
+		time.sleep(5)
+		GPIO.output(17,True)
+                time.sleep(1)
+                GPIO.output(17,False)
+                time.sleep(1)
+                GPIO.output(17,True)
+                time.sleep(1)
+                GPIO.output(17,False)
+                time.sleep(1)
+                GPIO.output(17,True)
+                time.sleep(1)
+                GPIO.output(17,False)
+		time.sleep(1)
+		GPIO.output(17,True)
+		time.sleep(1)
+		GPIO.output(17, False)
+                time.sleep(5)
+
+	print "done!!"
+	GPIO.cleanup()
+Blink()
